429311c78e9cf745f01d82c32d55eddb1cdd441d
Victor-Lis <victorlisbronzo1@gmail.com>
2024-04-13 22:56:39 -0300

    v1.13
    - Definindo função create para salvar uma clima no banco de dados

--- src/app/api/clima/route.ts
+++ src/app/api/clima/route.ts
@@ -2,13 +2,40 @@ import { NextResponse } from 'next/server'
 import prismaClient from '@/lib/prisma'
 import { redirect } from 'next/navigation'
 
-export async function GET(request: Request){
+import { getCityWeather } from './getCityWeather'
 
-    const { searchParams } = new URL(request.url)
+export async function POST(request: Request){
+    const { id_do_comodo, temperatura_do_comodo, humidade_do_comodo } = await request.json()
+
+    if(!id_do_comodo || !temperatura_do_comodo || !humidade_do_comodo){
+        return NextResponse.json({ error: "Precisamos do 'id_do_comodo', 'temperatura_do_comodo' e 'humidade_do_comodo'!" }, {status: 400})
+    }
 
     try {
-        return NextResponse.json("Clima Salvo!")
+        let comodo = await prismaClient.comodo.findFirst({
+            where: {
+                id: id_do_comodo,
+            }
+        })
+        if(!comodo){
+            return NextResponse.json({ error: "Cômodo não encontrado!" }, {status: 400})
+        }
+        let climaData = await getCityWeather(comodo?.cidade as string)
+        if(!climaData){
+            return NextResponse.json({ error: "Clima externo não encontrado!" }, {status: 400})
+        }
+        let clima = await prismaClient.climas.create({
+            data: {
+                id_do_comodo,
+                temperatura_do_comodo: parseFloat(temperatura_do_comodo),
+                humidade_do_comodo: parseFloat(humidade_do_comodo),
+                humidade_externa: climaData.main.humidity,
+                temperatura_externa: climaData.main.temp,
+            }
+        })
+        return NextResponse.json(clima)
     } catch (error) {
-        return NextResponse.json({ error: "Clima não criada!" }, {status: 400})
+        console.log(error)
+        return NextResponse.json({ error: "Clima não criado!" }, {status: 400})
     }
 }
