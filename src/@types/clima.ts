312cb4f21d5bcc1cc2a67fddaf9927c817436de8
Victor-Lis <victorlisbronzo1@gmail.com>
2024-04-14 15:41:58 -0300

    v1.18
    - Ajustando lib e tipagem "ClimaType"

--- src/@types/clima.ts
+++ src/@types/clima.ts
@@ -1,14 +1,14 @@
 import { ComodoType } from "./comodo";
 
-export type Clima = {
-    id: string;
+export type ClimaType = {
+    id: string,
 
-    temperatura_do_comodo: number;
-    temperatura_externa: number;
+    temperatura_do_comodo: number,
+    temperatura_externa: number,
 
-    humidade_do_comodo: number;
-    humidade_externa: number;
+    humidade_do_comodo: number,
+    humidade_externa: number,
 
-    id_do_comodo: number;
-    comodo: ComodoType
+    id_do_comodo: string,
+    comodo: ComodoType,
 }
