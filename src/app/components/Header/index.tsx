ae9d7942bc77705a0c639ed68a17b9570eeae190
Victor-Lis <victorlisbronzo1@gmail.com>
2024-04-16 10:45:11 -0300

    v1.30
    - Ajustando Header para poder ser reaproveitado

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -2,16 +2,16 @@
 import { useRouter } from "next/navigation";
 import { FiPlusCircle } from "react-icons/fi";
 
-export default function RegistersHeader() {
+export default function Header({title, route}:{title: string, route: string}) {
 
     const router = useRouter()
 
   return (
     <div className="px-2 flex justify-between items-center mt-2 mb-5">
-      <h2 className="text-3xl">Registros</h2>
+      <h2 className="text-3xl">{title}</h2>
       <FiPlusCircle 
         size={35} color="#06bb00" className="hover:opacity-75 hover:scale-90 duration-300 cursor-pointer" 
-        onClick={() => {router.push(`/cadastro`)}}
+        onClick={() => {router.push(route)}}
       />
     </div>
   );
