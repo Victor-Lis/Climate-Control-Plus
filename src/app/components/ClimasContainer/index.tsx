d6fd75f33b7af0dd64ba4be162100f0b26a66e7f
Victor-Lis <victorlisbronzo1@gmail.com>
2024-04-20 15:10:57 -0300

    v1.52
    - Adicionando humidade no gráfico

--- src/app/components/ClimasContainer/index.tsx
+++ src/app/components/ClimasContainer/index.tsx
@@ -9,7 +9,9 @@ export default function ClimasContainer() {
   const [labels, setLabels] = useState<string[]>([])
   const [temperaturaDoComodo, setTemperaturaDoComodo] = useState<number[]>([])
   const [temperaturaExterna, setTemperaturaExterna] = useState<number[]>([])
- 
+  const [humidadeDoComodo, setHumidadeDoComodo] = useState<number[]>([])
+  const [humidadeExterna, setHumidadeExterna] = useState<number[]>([])
+
   const formatNum = (n: number) => n < 10 ? "0"+n : n
   const formatDate = (date: Date) => `${formatNum(date.getDate())}/${formatNum(date.getMonth())}/${date.getFullYear()}`
 
@@ -23,6 +25,8 @@ export default function ClimasContainer() {
         setLabels((oldArray) => [formatDate(new Date(data.datetime)), ...oldArray])
         setTemperaturaDoComodo((oldArray) => [data.temperatura_do_comodo, ...oldArray])
         setTemperaturaExterna((oldArray) => [data.temperatura_externa, ...oldArray])
+        setHumidadeDoComodo((oldArray) => [data.humidade_do_comodo, ...oldArray])
+        setHumidadeExterna((oldArray) => [data.humidade_externa, ...oldArray])
       })
     }
     handeGetClimas()
@@ -38,13 +42,23 @@ export default function ClimasContainer() {
         datasets: [
           {
             label: "Temperatura do Cômodo",
-            borderColor: "rgb(75, 192, 192)",
+            borderColor: "rgb(0, 150, 255)",
             data: temperaturaDoComodo,
           },
           {
             label: "Temperatura Externa",
-            borderColor: "rgb(75, 192, 77)",
+            borderColor: "rgb(0, 0, 255)",
             data: temperaturaExterna,
+          },
+          {
+            label: "Humidade do Cômodo",
+            borderColor: "rgb(75, 192, 77)",
+            data: humidadeDoComodo,
+          },
+          {
+            label: "Humidade Externa",
+            borderColor: "rgb(1, 92, 1)",
+            data: humidadeExterna,
           },
         ]
       }}/>
